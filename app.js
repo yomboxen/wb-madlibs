caae237c143d4be7ac6840ef624d819a0af63f95
Luke Hardy <lukeclaytonhardy@gmail.com>
2023-08-15 14:51:31 -0600

    does a lot of stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample'
 
 const app = express();
 const port = '8000';
@@ -48,5 +49,20 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html.njk', { 
+    name: name,
+    compliment: compliment
+  });
 });
+
+app.get('/game', (req, res) => {
+  if(req.query.play === 'no')  {
+    res.render('goodbye.html.njk')
+  } else res.render('game.html.njk')
+})
+
+app.get('/madlib', (req, res) => {
+  res.render('madlib.html.njk') 
+  
+})
